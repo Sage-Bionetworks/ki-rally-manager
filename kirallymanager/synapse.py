34894426e375ac72df41a2eea777f815182e561b
kdaily <kenneth.daily@sagebase.org>
2019-08-21 11:43:29 -0700

    add code to provide synapse object

---
+++ kirallymanager/synapse.py
@@ -0,0 +1,72 @@
+# Copyright 2018-present, Bill & Melinda Gates Foundation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from .param_store import ParamStore
+import os
+import synapseclient
+
+class Synapse:
+
+    _synapse_client = None
+
+    ADMIN_PERMS = [
+        'UPDATE',
+        'DELETE',
+        'CHANGE_PERMISSIONS',
+        'CHANGE_SETTINGS',
+        'CREATE',
+        'DOWNLOAD',
+        'READ',
+        'MODERATE'
+    ]
+
+    CAN_EDIT_AND_DELETE_PERMS = [
+        'DOWNLOAD',
+        'UPDATE',
+        'CREATE',
+        'DELETE',
+        'READ'
+    ]
+
+    CAN_EDIT_PERMS = [
+        'DOWNLOAD',
+        'UPDATE',
+        'CREATE',
+        'READ'
+    ]
+
+    CAN_DOWNLOAD_PERMS = [
+        'DOWNLOAD',
+        'READ'
+    ]
+
+    CAN_VIEW_PERMS = [
+        'READ'
+    ]
+
+    @classmethod
+    def client(cls, tmp_cache_root=False):
+        """
+        Gets a logged in instance of the synapseclient.
+        """
+        if not cls._synapse_client:
+            cls._synapse_client = synapseclient.Synapse()
+            try:
+                cls._synapse_client.login(silent=True)
+            except:
+                syn_user = ParamStore.SYNAPSE_USERNAME()
+                syn_pass = ParamStore.SYNAPSE_PASSWORD()
+                cls._synapse_client.login(syn_user, syn_pass, silent=True)
+
+        return cls._synapse_client
